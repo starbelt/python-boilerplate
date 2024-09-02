3283ad21d7ca51bd06807f5877e54e36891a77a0
Brad Denby <bdenby@starbelt.net>
2024-09-02 13:48:51 -0400

    Adds Python boilerplate files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Python Boilerplate
+
+This repository contains a starting point for writing Python scripts.
+
+**What is boilerplate?** "Boilerplate" refers to text that can be reused without
+making significant changes.
+
+## Directory Contents
+
+* [python_boilerplate.py](python_boilerplate.py): Python boilerplate script
+* [LICENSE](LICENSE): The license for this repository
+* [README.md](README.md): This document
+
+## License
+
+Written by Bradley Denby  
+Other contributors: None
+
+See the top-level LICENSE file for the license.
---
+++ python_boilerplate.py
@@ -0,0 +1,46 @@
+# script_name.py
+#
+# Usage: python3 script_name.py arg1 arg2 ...
+#  Text explaining script usage
+# Parameters:
+#  arg1: description of argument 1
+#  arg2: description of argument 2
+#  ...
+# Output:
+#  A description of the script output
+#
+# Written by First Last
+# Other contributors: None
+#
+# Optional license statement, e.g., See the LICENSE file for the license.
+
+# import Python modules
+# e.g., import math # math module
+import sys # argv
+
+# "constants"
+# e.g., R_E_KM = 6378.137
+
+# helper functions
+
+## function description
+# def calc_something(param1, param2):
+#   pass
+
+# initialize script arguments
+# arg1 = '' # description of argument 1
+# arg2 = '' # description of argument 2
+
+# parse script arguments
+# if len(sys.argv)==3:
+#   arg1 = sys.argv[1]
+#   arg2 = sys.argv[2]
+#   ...
+# else:
+#   print(\
+#    'Usage: '\
+#    'python3 arg1 arg2 ...'\
+#   )
+#   exit()
+
+# write script below this line
